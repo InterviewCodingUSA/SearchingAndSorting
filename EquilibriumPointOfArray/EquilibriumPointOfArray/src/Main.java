2636b95cf5879325c8176176f98746f35ee824ef
Ashish <ashishsingh@gmail.com>
2025-09-15 15:42:51 -0700

    Checking in code for equilibrium point of array

---
+++ EquilibriumPointOfArray/EquilibriumPointOfArray/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {-7,1,5,2,-4,3,0};
+        System.out.println( equilibriumPointOfArray(arr) );
+    }
+    /// <summary>
+    /// index where sum of left and right are same
+    /// -7,1,5,2,-4,3,0
+    /// index = 3
+    public static int equilibriumPointOfArray(int[] arr)
+    {
+        int sum = 0;      // initialize sum of whole array
+        int leftsum = 0; // initialize leftsum
+
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
+        for (int i = 0; i < arr.length; i++)
+        {
+            sum = sum - arr[i];
+            if (leftsum == sum)
+                return i;
+            leftsum += arr[i];
+        }
+        return -1;
+    }
+
+}
